5d793c1ea09cd8f890a6b11cfe5d8f47c04cc695
NorthSeaPark <33457633+NorthSeaPark@users.noreply.github.com>
2019-10-17 16:23:18 -0400

    fuck

--- Linked_list.c
+++ Linked_list.c
@@ -9,16 +9,18 @@ struct linkedlist{
 
 typedef struct linkedlist *node; /*define the node*/
 
-node createNode(){
+node createNode(int x){
 	node temp;
 	temp = (node)malloc(sizeof(struct linkedlist));
+	temp->data = x;
 	temp->next = NULL;
 	return temp;
 }
 
+/*Simply put the node one by one*/
 node addNode(node head, int value){
 	node temp, p;
-	temp = createNode();
+	temp = createNode(1);
 	temp->data = value;
 	if(head == NULL){
 		head = temp;
@@ -32,19 +34,38 @@ node addNode(node head, int value){
 	return head;
 }
 
+/*Insert by order*/
+void pushNode(node head, int value){
+	node temp = createNode(value);
+	if(head->next == NULL){
+		if(value>=head->data){
+			head->next = temp;
+		}else{
+			temp->next=head;
+			head = temp;
+		}
+	}else{
+		node current = head;
+		while(current->next!=NULL&&current->next->data<value){
+			current = current->next;
+		}
+		temp->next=current->next;
+		current->next = temp;
+	}
+
+}
+
 void traverse(node head){
 	node temp = head;
 	while(temp != NULL){
-		temp = temp->next;
 		printf("%d\n",temp->data);
+		temp = temp->next;
 	}
 }
 
 int main()
 {
-	node sb = createNode();
-	sb = addNode(sb, 3);
-	sb = addNode(sb, 2);
-	sb = addNode(sb, 1);
+	node sb = createNode(4);
+	pushNode(sb,3);
 	traverse(sb);
 }
