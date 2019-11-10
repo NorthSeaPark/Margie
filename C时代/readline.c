a146ee3bcf1cd923c190fae6901b5b01cdf889e9
NorthSeaPark <33457633+NorthSeaPark@users.noreply.github.com>
2019-11-10 16:37:18 -0500

    No moive tonight

--- C时代/readline.c
+++ C时代/readline.c
@@ -7,4 +7,13 @@ int read_line(char str[], int n){
 	while(isspace(ch = getchar()))
 		;
 	/*isspace用来判断他的实际参数是否是空白字符*/
+	while(ch!='\n'&&ch!=EOF){
+		if(i<n){
+			str[i++] = ch;
+		}
+		ch = getchar();
+	}
+	str[i] = '\0';
+	return i;
 }
+/*\0 is always at the end of a string*/
